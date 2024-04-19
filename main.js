5b7251aea8f4ce8d22bcf201997fd7b20f7d33d6
unknown <you@example.com>
2024-04-19 22:03:02 +0500

    Adding a Unary operators

---
+++ main.js
@@ -0,0 +1,8 @@
+// UNARY OPERATORS ASSIGNMENT
+let a = 5;
+let b = 2;
+let c;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+// NOW PRINT THE OUTPUT 
+console.log(c);
+export {};
---
+++ main.ts
@@ -0,0 +1,9 @@
+// UNARY OPERATORS ASSIGNMENT
+
+let a:number = 5;
+let b:number =2;
+let c:number;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+
+// NOW PRINT THE OUTPUT 
+console.log(c);
